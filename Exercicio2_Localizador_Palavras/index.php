77f68b33919d5bfd95366b3341ad55b06ba299be
HenriqueBraz <henrique.braz@acad.pucrs.br>
2018-10-17 00:49:40 -0300

    Update index.php

--- Exercicio2_Localizador_Palavras/index.php
+++ Exercicio2_Localizador_Palavras/index.php
@@ -1,12 +1,7 @@
 
 <?php
 
-	//$cont = 0;
-
-	//$arr = array($_POST["nome1"],$_POST["nome2"],$_POST["nome3"],$_POST["nome4"],$_POST["nome5"]);
- 
-          $frase = array($_POST["frase"],$_POST["busca" ],$_POST["substituir"]);
-
+	
           $frase = array($_POST["frase"]);
           $busca = $_POST["busca"];
           $subst = $_POST["substituir"];
@@ -21,10 +16,6 @@
 	}
 
          
-        
-    	
-    
-
 ?>
 
 
