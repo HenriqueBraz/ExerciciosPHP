24bcdc52195fa239595efab1f0620cae053f7e6a
HenriqueBraz <henrique.braz@acad.pucrs.br>
2018-10-17 01:53:37 -0300

    Update index.php

--- Exercicio2_Localizador_Palavras/index.php
+++ Exercicio2_Localizador_Palavras/index.php
@@ -1,21 +1,36 @@
 
 <?php
 
-	
-          $frase = array($_POST["frase"]);
+	         
+
+          $frase = $_POST["frase"];
           $busca = $_POST["busca"];
           $subst = $_POST["substituir"];
 
+          $pos = stripos($frase, $busca);
 
-          $novaFrase = str_ireplace($busca,$subst,$frase);
+
+          if(stripos($frase, $busca) == true){
+
+	    	$novaFrase = str_ireplace($busca,$subst,$frase);
+                echo("O ítem '$busca' está localizado na '$pos' posição da frase: '$frase'");
+        	echo'<br>';
+        	echo("Modificando '$busca' por '$subst':");
+        	echo'<br>';
+        	echo($novaFrase);
 
 
-          foreach ( $novaFrase as $a){
+	  }
 
-	    echo($a);
-	}
+	  else{
 
-         
+
+                echo("Não foi posível encontrar a palavra '$busca' em '$frase'");
+
+          }
+      
+      
+
 ?>
 
 
